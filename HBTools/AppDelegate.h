6ab7c5831cc62629d36f755fee569fb6f034439a
hengbin <Dchaoliu@163.com>
2018-12-26 18:03:09 +0800

    Initial Commit

---
+++ HBTools/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HBTools
+//
+//  Created by 花生酱 on 26/12/2018.
+//  Copyright © 2018 花生酱. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HBTools/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  HBTools
+//
+//  Created by 花生酱 on 26/12/2018.
+//  Copyright © 2018 花生酱. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
